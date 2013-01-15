795fedceb8882a442e5ac02f5657df38eaa86f70
SteveCossell <steve@scriptrock.com>
2013-01-15 23:40:45 +1100

    Added proper roles via comms to sc0011

--- sc0011/RobotPlayer.java
+++ sc0011/RobotPlayer.java
@@ -8,20 +8,49 @@ import battlecode.common.RobotController;
 import battlecode.common.RobotType;
 import battlecode.common.Upgrade;
 
-public class RobotPlayer {
+public class RobotPlayer {    
     public static void run(RobotController rc) {
 	while (true) {
 	    try {
 		if (rc.getType() == RobotType.HQ) {
 		    HQPlayer(rc);
-		} else if (rc.getType() == RobotType.SOLDIER) {		    
+		} else if (rc.getType() == RobotType.SOLDIER) {
+		    int global_task = getRobotTask(rc, 1);		    
 		    int task = getRobotTask(rc, rc.getRobot().getID());
 		    
-		    if (task == ROBOT_TASK_UNKNOWN) {
-			setRobotTask(rc, rc.getRobot().getID(), ROBOT_TASK_SCOUT);
+		    if (Clock.getRoundNum() > 600) {
 			ScoutPlayer(rc);
-		    } else if (task == ROBOT_TASK_ERROR) {
-			ScoutPlayer(rc);
+			
+		    } else {
+
+			if (global_task == ROBOT_TASK_UNKNOWN || global_task == ROBOT_TASK_SCOUT) {
+			    if (task == ROBOT_TASK_UNKNOWN) {
+				System.out.println("Made SCOUT (crisis)");
+				setRobotTask(rc, rc.getRobot().getID(), ROBOT_TASK_SCOUT);
+				ScoutPlayer(rc);
+			    } else if (task == ROBOT_TASK_ERROR) {
+				ScoutPlayer(rc);
+			    }
+			} else {
+			    if (task == ROBOT_TASK_UNKNOWN) {
+				int rand = getRobotTask(rc, 0);
+				setRobotTask(rc, 0, rand+1);
+				
+				if (rand % 3 == 0) {
+				    System.out.println("Made SCOUT");
+				    setRobotTask(rc, rc.getRobot().getID(), ROBOT_TASK_SCOUT);
+				    ScoutPlayer(rc);
+				} else {
+				    System.out.println("Made MINER");
+				    setRobotTask(rc, rc.getRobot().getID(), ROBOT_TASK_MINER);
+				    MinerPlayer(rc);
+				}
+			    } else if (task == ROBOT_TASK_MINER) {
+				MinerPlayer(rc);
+			    } else {
+				ScoutPlayer(rc);
+			    }
+			}
 		    }
 		}
 
@@ -35,8 +64,18 @@ public class RobotPlayer {
     
     public static void HQPlayer(RobotController rc) throws Exception {
 	if (rc.isActive()) {
-			
-	    if (rc.hasUpgrade(Upgrade.valueOf("DEFUSION")) || Clock.getRoundNum() < 10) {
+		
+	    int clock_round = Clock.getRoundNum();
+
+	    if (clock_round % 50 == 0) {
+		if (rc.senseEnemyNukeHalfDone()) {
+		    setRobotTask(rc, 1, ROBOT_TASK_SCOUT);
+		} else {
+		    setRobotTask(rc, 1, ROBOT_TASK_MINER);
+		}
+	    }
+	
+	    if (rc.hasUpgrade(Upgrade.valueOf("DEFUSION")) || clock_round < 10) {
 			    
 		// Spawn a soldier
 		Direction dir = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
@@ -147,6 +186,46 @@ public class RobotPlayer {
 	}	
     }
     
+    public static void MinerPlayer(RobotController rc) throws Exception {
+	if (rc.isActive()) {
+	    MapLocation hq = rc.senseHQLocation();	    
+	    
+	    int dist_to_hq = rc.getLocation().distanceSquaredTo(hq);
+	    
+	    int clock_round = Clock.getRoundNum();
+	    
+	    int desired_radius = (clock_round / 100) + 1;
+
+	    if (inRange(dist_to_hq-1, desired_radius * desired_radius, dist_to_hq+1)) {
+		if (rc.senseMine(rc.getLocation()) == null) {
+		    //rc.layMine();
+		    return;
+		}
+	    }
+	    
+	    Direction dir = rc.getLocation().directionTo(hq);
+	    dir = dir.rotateLeft().rotateLeft();
+	    
+	    for (int i = 0; i < 8; i++) {
+	    
+		MapLocation dir_loc = rc.getLocation().add(dir);	       
+		
+		if (rc.senseMine(dir_loc) == null) {
+		    if (rc.canMove(dir)) {
+			rc.move(dir);
+			return;
+		    }
+		}
+		
+		dir = dir.rotateRight();
+	    }
+	}
+    }
+    
+    public static boolean inRange(int a, int n, int b) {
+	return a<=n && n <= b;
+    }
+    
     public static final int HASH1 = 13;
     public static final int HASH2 = 29;
     public static final int HASH3 = 131;
@@ -155,23 +234,33 @@ public class RobotPlayer {
     public static final int ROBOT_TASK_UNKNOWN = 0;
     public static final int ROBOT_TASK_SCOUT = 1;
     public static final int ROBOT_TASK_MINER = 2;
+    public static final int ROBOT_TASK_GUARD = 3;
 
-    public static void setRobotTask(RobotController rc, int robot_id, int task) throws Exception {
-	rc.broadcast(HASH1 * robot_id, task * HASH3);
-	rc.broadcast(HASH2 * robot_id, task * HASH1);
-	rc.broadcast(HASH3 * robot_id, task * HASH2);
+    public static String taskToString(int val) {
+	if (val == ROBOT_TASK_ERROR) return "ROBOT_TASK_ERROR";
+	if (val == ROBOT_TASK_UNKNOWN) return "ROBOT_TASK_UNKNOWN";
+	if (val == ROBOT_TASK_SCOUT) return "ROBOT_TASK_SCOUT";
+	if (val == ROBOT_TASK_MINER) return "ROBOT_TASK_MINER";
+	if (val == ROBOT_TASK_GUARD) return "ROBOT_TASK_GUARD";
+	return "Unknown";
     }
 
-    public static int getRobotTask(RobotController rc, int robot_id) throws Exception {
-	int val1 = rc.readBroadcast(robot_id / HASH1) / HASH3;
-	int val2 = rc.readBroadcast(robot_id / HASH2) / HASH1;
-	int val3 = rc.readBroadcast(robot_id / HASH3) / HASH2;
+    public static void setRobotTask(RobotController rc, int robot_id, int task) throws Exception {	
+	rc.broadcast(HASH1 * robot_id, task * HASH3);
+	rc.broadcast(HASH2 * robot_id, task * HASH1);
+	rc.broadcast(HASH3 * robot_id, task * HASH2);	
+    }
+
+    public static int getRobotTask(RobotController rc, int robot_id) throws Exception {		
+	int val1 = rc.readBroadcast(robot_id * HASH1) / HASH3;
+	int val2 = rc.readBroadcast(robot_id * HASH2) / HASH1;
+	int val3 = rc.readBroadcast(robot_id * HASH3) / HASH2;
+
+	if (val1 == val2) return val1;
+	if (val1 == val3) return val1;
+	if (val2 == val3) return val2;
 	
-	if (val1 == val2 && val2 == val3) {
-	    return val1;
-	} else {
-	    return ROBOT_TASK_ERROR;
-	}
+	return ROBOT_TASK_ERROR;
     }
     
 }
