f564a872a763a749b9c37e8b453cf65aa3437c9a
Rupert Shuttleworth <optimuscoprime@gmail.com>
2013-01-16 23:06:53 +1100

    clean up debug player a bit

--- oc001/RobotPlayer.java
+++ oc001/RobotPlayer.java
@@ -2,26 +2,27 @@ package oc001;
 
 import battlecode.common.RobotController;
 
-public class RobotPlayer {    
-	
+public class RobotPlayer {    		
 	public static void run(RobotController rc) {
-		//	System.setProperty("debugMethodsEnabled", "true");
-		// use bc.conf to turn debug mode on
-		
 		while(true) {
 			try {
-				
-				throw new Exception("TODO: make strategy");
-				
+				playOneTurn(rc);
+				rc.yield();		
 			} catch (Exception e) {
 				debug_printf(e.toString());
 			}
 		}
 	}
-	
-	public static void debug_printf(String format, Object ... objects) {
+		
+   //	System.setProperty("debugMethodsEnabled", "true");
+   // use bc.conf to turn debug mode on	
+	private static void debug_printf(String format, Object ... objects) {
 		System.out.printf(format, objects);
 		System.out.printf("\n");
+	}	
+	
+	private static void playOneTurn(RobotController rc) {
+		throw new RuntimeException("TODO: make strategy");
 	}	
 }
 
