0d0a6c80291c7aaac3bb1d89b617b74f06a83127
Rupert Shuttleworth <optimuscoprime@gmail.com>
2013-01-17 11:34:15 +1100

    avoid assert not moving exception, build medbays, bigger radius to sense enemies

--- team219/RobotPlayer.java
+++ team219/RobotPlayer.java
@@ -11,11 +11,14 @@ import battlecode.common.GameObject;
 import battlecode.common.MapLocation;
 import battlecode.common.Robot;
 import battlecode.common.RobotController;
+import battlecode.common.RobotType;
 import battlecode.common.Team;
 import battlecode.common.Upgrade;
 
 public class RobotPlayer {    	
 	private final static double MIN_TEAM_POWER = 60.0;
+	private final static int SENSE_ENEMY_RADIUS = 120;
+	private final static int SENSE_FRIENDLY_RADIUS = 1;
 	
 	public static void run(RobotController rc) {
 		while(true) {
@@ -23,7 +26,7 @@ public class RobotPlayer {
 				playOneTurn(rc);
 				rc.yield();		
 			} catch (Exception e) {
-				debug_printf(e.toString());
+				debug_printf(e);
 			}
 		}
 	}
@@ -36,40 +39,43 @@ public class RobotPlayer {
 	}	
 	
 	private static void debug_printf(Exception e) {
-		System.out.printf(e.getMessage());
+		System.out.printf("%s: %s", e.getStackTrace()[0].getMethodName(), e.getMessage());
 		System.out.printf("\n");
 	}	
 
 	private static void playOneTurn(RobotController rc) {
-		switch(rc.getType()) {
-			case HQ:
-				playOneTurn_hq(rc);
-				break;		
-			case SOLDIER:
-				playOneTurn_soldier(rc);
-				break;			
-			case SUPPLIER:
-				playOneTurn_supplier(rc);
-				break;
-			case ARTILLERY:
-				playOneTurn_artillery(rc);
-				break;
-			case GENERATOR:
-				playOneTurn_generator(rc);
-				break;
-			case MEDBAY:
-				playOneTurn_medbay(rc);
-				break;
-			case SHIELDS:
-				playOneTurn_shields(rc);
-				break;
+		if (rc.isActive()) {
+			switch(rc.getType()) {
+				case HQ:
+					playOneTurn_hq(rc);
+					break;		
+				case SOLDIER:
+					playOneTurn_soldier(rc);
+					break;			
+				case SUPPLIER:
+					playOneTurn_supplier(rc);
+					break;
+				case ARTILLERY:
+					playOneTurn_artillery(rc);
+					break;
+				case GENERATOR:
+					playOneTurn_generator(rc);
+					break;
+				case MEDBAY:
+					playOneTurn_medbay(rc);
+					break;
+				case SHIELDS:
+					playOneTurn_shields(rc);
+					break;
+			}
 		}
 	}
 
 	private static void playOneTurn_hq(RobotController rc) {
 		double power = rc.getTeamPower();
+				
 		// try to spawn a soldier
-		
+				
 		if (power > MIN_TEAM_POWER) {
 			Direction spawnDirection = findBestSpawnDirection(rc);
 			if (spawnDirection != Direction.NONE && couldSpawnSoldier(rc, spawnDirection)) {
@@ -146,29 +152,74 @@ public class RobotPlayer {
 	}	
 
 	private static void playOneTurn_soldier(RobotController rc) {
-		Direction direction = findBestDirectionToMove(rc);
+		MapLocation myLocation = rc.getLocation();		
+		
+		Direction direction = findBestDirectionToMove(rc, myLocation);
 
+		// try to build encampments
+		
+		if (rc.senseEncampmentSquare(myLocation)) {
+			RobotType encampmentTypes[] = new RobotType[]{
+					RobotType.MEDBAY, // assume medbay is the best
+					RobotType.SHIELDS,
+					RobotType.GENERATOR,
+					RobotType.ARTILLERY,
+					RobotType.SUPPLIER
+			};
+			for (RobotType encampmentType: encampmentTypes) {
+				if (shouldBuildEncampment(rc, encampmentType, myLocation) && couldBuildEncampment(rc, encampmentType)) {
+					return;
+				}				
+			}
+		}	
+		
+		// try to plant a mine
+		
+		if (rc.senseMine(myLocation) == null) {
+			if (couldLayMine(rc)) {
+				return;
+			}
+		}		
+		
 		// try to move
 		
-		if (direction != Direction.NONE) {
+		if (direction != null) {
 			if (couldMove(rc, direction)) {
 				return;
 			}
 		}
 		
-		// try to plant a mine
-		
-		MapLocation myLocation = rc.getLocation();
-		if (rc.senseMine(myLocation) == null) {
-			try {
-				rc.layMine();
-			} catch (GameActionException e) {
-				debug_printf(e);
-			}
-		}
-		
-		// TODO: should try to defuse, or try to build encampments, etc.
+		// TODO: should try to defuse, etc
 	}	
+	
+	private static boolean shouldBuildEncampment(RobotController rc, RobotType encampmentType, MapLocation myLocation) {
+		// TODO: better logic to figure out when to build
+		return true;
+	}
+
+	private static boolean couldBuildEncampment(RobotController rc, RobotType encampmentType) {
+		boolean builtEncampment = false;
+		try {
+			rc.captureEncampment(encampmentType);
+			builtEncampment = true;
+		} catch (GameActionException e) {
+			debug_printf(e);
+			builtEncampment = false;
+		}
+		return builtEncampment;
+	}
+
+	private static boolean couldLayMine(RobotController rc) {
+		boolean mined = false;
+		try {
+			rc.layMine();
+			mined = true;
+		} catch (GameActionException e) {
+			debug_printf(e);
+			mined = false;
+		}
+		return mined;
+	}
 
 	private static boolean couldMove(RobotController rc, Direction direction) {
 		boolean moved = false;
@@ -182,10 +233,9 @@ public class RobotPlayer {
 		return moved;
 	}
 	
-	private static Direction findBestDirectionToMove(RobotController rc) {
-		Direction bestDirection = Direction.NONE;
+	private static Direction findBestDirectionToMove(RobotController rc, MapLocation myLocation) {
+		Direction bestDirection = null;
 		Team myTeam = rc.getTeam();
-		MapLocation myLocation = rc.getLocation();
 		
 		Robot enemyRobots[] = getNearbyEnemies(rc);
 		Robot friendlyRobots[] = getNearbyFriendlies(rc);
@@ -194,12 +244,15 @@ public class RobotPlayer {
 			List<Direction> prioritisedDirections = getPrioritisedDirections(rc);
 			
 			for (Direction direction: prioritisedDirections) {
-				MapLocation newLocation = myLocation.add(direction);
-				Team mineTeam = rc.senseMine(newLocation);
-				boolean safeDirection = (mineTeam == null || mineTeam == myTeam);
-				if (safeDirection && rc.canMove(direction)) {
-					bestDirection = direction;
-					break;
+				// causes exception
+				if (direction != Direction.OMNI && direction != Direction.NONE) {
+					MapLocation newLocation = myLocation.add(direction);
+					Team mineTeam = rc.senseMine(newLocation);
+					boolean safeDirection = (mineTeam == null || mineTeam == myTeam);
+					if (safeDirection && rc.canMove(direction)) {
+						bestDirection = direction;
+						break;
+					}
 				}
 			}			
 		}
@@ -218,6 +271,7 @@ public class RobotPlayer {
 			final MapLocation myLocation = rc.getLocation();
 			
 			// sort based on distance to enemy
+			// TODO: this is a really inefficient way of sorting
 			Collections.sort(directions, new Comparator<Direction>() {
 				@Override
 				public int compare(Direction o1, Direction o2) {
@@ -256,12 +310,12 @@ public class RobotPlayer {
 	
 	private static Robot[] getNearbyEnemies(RobotController rc) {
 		Team otherTeam = rc.getTeam().opponent();
-		return rc.senseNearbyGameObjects(Robot.class, 30, otherTeam);
+		return rc.senseNearbyGameObjects(Robot.class, SENSE_ENEMY_RADIUS, otherTeam);
 	}
 	
 	private static Robot[] getNearbyFriendlies(RobotController rc) {
 		Team myTeam = rc.getTeam();
-		return rc.senseNearbyGameObjects(Robot.class, 1, myTeam);
+		return rc.senseNearbyGameObjects(Robot.class, SENSE_FRIENDLY_RADIUS, myTeam);
 	}	
 
 	private static void playOneTurn_shields(RobotController rc) {
